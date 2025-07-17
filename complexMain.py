06536b2de9a01b591b9161840849904601a08deb
Santhusha-bit <santhushajanana@gmail.com>
2025-07-17 01:16:29 -0500

    Project Setup

---
+++ complexMain.py
@@ -0,0 +1,40 @@
+from huggingface_hub.inference._generated.types import summarization
+from transformers.pipelines import pipeline
+from langchain_huggingface import HuggingFacePipeline
+from langchain.prompts import PromptTemplate
+from transformers.utils.logging import set_verbosity_error
+
+set_verbosity_error()
+
+summarization_pipeline = pipeline(task="summarization", model="facebook/bart-large-cnn", device="cpu")
+summarizer = HuggingFacePipeline(pipeline=summarization_pipeline)
+
+refinement_pipeline = pipeline(task="summarization", model="facebook/bart-large", device="cpu")
+refiner = HuggingFacePipeline(pipeline=refinement_pipeline)
+
+qa_pipeline = pipeline(task="question-answering", model="deepset/roberta-base-squad2", device="cpu")
+
+summary_template = PromptTemplate.from_template("Summarize the following text in a {length} way:\n\n{text}")
+
+summarization_chain = summary_template | summarizer | refiner
+
+text_to_summerize = input("\nEnter text to summarize:\n")
+
+length = input("\nEnter the length (short/medium/long): ")
+length_map = {"short": 50, "medium": 150, "long": 300}
+max_length = length_map.get(length.lower(), 150)
+
+summary = summarization_chain.invoke({"text": text_to_summerize, "lenght": max_length})
+
+print("\n ** Generate Summary **")
+print(summary)
+
+while True:
+    question = input("\nAsk a question about the summary (or type 'exit' to stop):\n")
+    if question.lower() == exit:
+        break
+
+    qa_result = qa_pipeline(question=question, context=summary)
+
+    print("\n ** Answer **")
+    print(qa_result["answer"])
---
+++ simpleMain.py
@@ -0,0 +1,29 @@
+from transformers.pipelines import pipeline
+from langchain_huggingface import HuggingFacePipeline
+from langchain.prompts import PromptTemplate
+
+# import torch
+
+# Check of the GPU is available
+# print(torch.cuda.is_available())
+# print(torch.cuda.get_device_name(0))
+
+model = pipeline(task="text-generation", 
+                model="mistralai/Mistral-7B-Instruct-v0.1", 
+                device="cpu",
+                max_lenght=256,
+                truncation=True)
+
+
+llm = HuggingFacePipeline(pipeline=model)
+
+# Create prompt template
+template = PromptTemplate.from_template("Explain {topic} in detail for a {age} year old to understand")
+
+chain = template | llm
+topic = input("Topic: ")
+age = input("Age: ")
+
+# Execute the chain
+response = chain.invoke({"topic": topic, "age": age})
+print(response)
